ff688152d002c308675dd97a78e4f6811a6ecc8b
David Deck <deck.david@gmail.com>
2020-11-28 20:33:41 +0100

    Create test2.groovy

---
+++ test2.groovy
@@ -0,0 +1,6 @@
+import java.sql.*
+
+Connection connection = DriverManager.getConnection('jdbc:postgresql://gcav708:5432/dataflow', 'root', 'rootpw')
+connection.close
+
+return "Hello ${payload}2 !"
